6f9948a884bf8ba89aed95d8a197d1f2200d5c08
AJ Aberdein <ashleylouis.aberdein@vcontractor.co.za>
2021-09-07 18:52:27 +0200

    first commit

---
+++ app.js
@@ -0,0 +1,37 @@
+
+import express from "express";
+import { config } from "./config.js";
+
+const port = 4000;
+const app = express();
+import axios from "axios";
+import { constructHeaders, send } from "./services/alipay-client-service.js";
+import fs from "fs";
+import { verify, verifySignature } from "./services/signature-service.js";
+
+import * as path from "path";
+
+import { dirname } from "path";
+import { fileURLToPath } from "url";
+import { dateToLocalISO, addMinutes } from "./services/date-time.js";
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
+
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "./app.html"));
+});
+
+app.get("/auth", (req, res) => {
+  res.send("success");
+});
+
+app.get("/pay", (req, res) => {
+  res.send("success");
+});
+
+app.listen(port, () => {
+  console.log(`listening on http://localhost:${port}`);
+});
+
